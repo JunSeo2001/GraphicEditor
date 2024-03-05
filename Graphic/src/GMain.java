a7a7bfba2866fe6d3428ddf1847a2a88af921001
JunSeo2001 <162302440+JunSeo2001@users.noreply.github.com>
2024-03-05 20:28:01 +0900

    2024/0305
    첫 커밋

---
+++ Graphic/src/GMain.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class GMain {
+    public static void main(String[] args) {
+        GFrame frame = new GFrame();
+        frame.frame();
+
+        
+
+    }
+}
